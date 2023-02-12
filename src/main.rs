af6ce5f720033b6589123d78893208a00f10f55e
mineiwik <29404520+mineiwik@users.noreply.github.com>
2023-02-12 20:14:32 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,456 @@
+extern crate mat4;
+use js_sys::{Uint8Array, WebAssembly};
+use std::cell::RefCell;
+use std::f32::consts::PI;
+use std::rc::Rc;
+use wasm_bindgen::prelude::*;
+use web_sys::{
+    console, Event, EventTarget, FileReader, HtmlDivElement, HtmlInputElement, MouseEvent,
+    WebGlBuffer, WebGlProgram, WebGlRenderingContext, WebGlShader, WebGlUniformLocation,
+};
+
+mod stl;
+
+const AMORTIZATION: f32 = 0.95;
+
+#[derive(Debug, Clone)]
+struct ProgramInfo(
+    WebGlProgram,
+    u32,
+    (
+        Result<WebGlUniformLocation, String>,
+        Result<WebGlUniformLocation, String>,
+    ),
+);
+#[derive(Debug, Clone)]
+struct Buffers(WebGlBuffer, WebGlBuffer);
+
+fn main() {
+    set_panic_hook();
+    set_file_reader().unwrap()
+}
+
+fn set_file_reader() -> Result<(), JsValue> {
+    let document = window()
+        .document()
+        .expect("should have a document on window");
+    let file_in_div = document.get_element_by_id("file-in").unwrap();
+    let file_in_div: HtmlDivElement = file_in_div.dyn_into()?;
+
+    let fileinput: HtmlInputElement = document
+        .create_element("input")?
+        .dyn_into::<HtmlInputElement>()?;
+
+    fileinput.set_id("file-upload");
+    fileinput.set_type("file");
+
+    let filereader = FileReader::new()?;
+
+    let closure = Closure::wrap(Box::new(move |event: Event| {
+        let element = event.target().unwrap().dyn_into::<FileReader>().unwrap();
+        let buffer = Uint8Array::new(&element.result().unwrap());
+        let v = buffer.to_vec();
+
+        match stl::get_data(&v) {
+            Ok((vertices, num_vertices)) => another(vertices, num_vertices).unwrap(),
+            Err(e) => console::log_1(&format!("The given file is corrupted: Error: {}", e).into()),
+        }
+    }) as Box<dyn FnMut(_)>);
+
+    filereader.set_onloadend(Some(closure.as_ref().unchecked_ref()));
+    closure.forget();
+
+    let closure = Closure::wrap(Box::new(move |event: Event| {
+        let element = event
+            .target()
+            .unwrap()
+            .dyn_into::<HtmlInputElement>()
+            .unwrap();
+        let filelist = element.files().unwrap();
+        let file = filelist.get(0).expect("should have a file handle.");
+        filereader.read_as_array_buffer(&file).unwrap();
+    }) as Box<dyn FnMut(_)>);
+
+    fileinput.add_event_listener_with_callback("change", closure.as_ref().unchecked_ref())?;
+    closure.forget();
+
+    file_in_div.append_child(&fileinput)?;
+    Ok(())
+}
+
+fn another(vertices: Vec<f32>, num_vertices: u32) -> Result<(), JsValue> {
+    let document = window()
+        .document()
+        .expect("should have a document on window");
+
+    let canvas = document.get_element_by_id("canvas").unwrap();
+    let canvas: web_sys::HtmlCanvasElement = canvas.dyn_into::<web_sys::HtmlCanvasElement>()?;
+
+    let gl = canvas
+        .get_context("webgl")?
+        .unwrap()
+        .dyn_into::<WebGlRenderingContext>()?;
+
+    let vertex_shader_source = r#"
+        attribute vec4 aVertexPosition;
+        uniform mat4 uModelViewMatrix;
+        uniform mat4 uProjectionMatrix;
+
+        varying lowp vec4 vColor;
+        
+        void main(void) {
+            gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+            vColor = aVertexPosition;
+        }
+    "#;
+
+    let fragment_shader_source = r#"
+        varying lowp vec4 vColor;
+        void main(void) {
+            gl_FragColor = vColor;
+        }
+    "#;
+
+    let shader_program = initShaderProgram(&gl, vertex_shader_source, fragment_shader_source)?;
+
+    let programm_info = {
+        let vertex_pos = gl.get_attrib_location(&shader_program, "aVertexPosition") as u32;
+        let projection_matrix = gl
+            .get_uniform_location(&shader_program, "uProjectionMatrix")
+            .ok_or_else(|| String::from("cannot get uProjectionMatrix"));
+        let model_view_matric = gl
+            .get_uniform_location(&shader_program, "uModelViewMatrix")
+            .ok_or_else(|| String::from("cannot get uModelViewMatrix"));
+        ProgramInfo(
+            shader_program,
+            vertex_pos,
+            (projection_matrix, model_view_matric),
+        )
+    };
+
+    // objects we'll be drawing.
+    let buffers: Buffers = initBuffers(&gl, vertices, num_vertices)?;
+
+    // Draw the scene repeatedly
+    let f = Rc::new(RefCell::new(None));
+    let g = f.clone();
+    let drag = Rc::new(RefCell::new(false));
+    let theta = Rc::new(RefCell::new(0.0));
+    let phi = Rc::new(RefCell::new(0.0));
+    let dx = Rc::new(RefCell::new(0.0));
+    let dy = Rc::new(RefCell::new(0.0));
+    let canvas_width = Rc::new(RefCell::new(canvas.width() as f32));
+    let canvas_height = Rc::new(RefCell::new(canvas.height() as f32));
+
+    // get canvas as event target
+    let event_target: EventTarget = canvas.into();
+
+    // Add event listeners
+    // MOUSEDOWN
+    {
+        let drag = drag.clone();
+        let mousedown_cb = Closure::wrap(Box::new(move |_event: MouseEvent| {
+            *drag.borrow_mut() = true;
+        }) as Box<dyn FnMut(MouseEvent)>);
+        event_target
+            .add_event_listener_with_callback("mousedown", mousedown_cb.as_ref().unchecked_ref())
+            .unwrap();
+        mousedown_cb.forget();
+    }
+    // MOUSEUP and MOUSEOUT
+    {
+        let drag = drag.clone();
+        let mouseup_cb = Closure::wrap(Box::new(move |_event: MouseEvent| {
+            *drag.borrow_mut() = false;
+        }) as Box<dyn FnMut(MouseEvent)>);
+        event_target
+            .add_event_listener_with_callback("mouseup", mouseup_cb.as_ref().unchecked_ref())
+            .unwrap();
+        event_target
+            .add_event_listener_with_callback("mouseout", mouseup_cb.as_ref().unchecked_ref())
+            .unwrap();
+        mouseup_cb.forget();
+    }
+    // MOUSEMOVE
+    {
+        let theta = theta.clone();
+        let phi = phi.clone();
+        let canvas_width = canvas_width.clone();
+        let canvas_height = canvas_height.clone();
+        let dx = dx.clone();
+        let dy = dy.clone();
+        let drag = drag.clone();
+        let mousemove_cb = Closure::wrap(Box::new(move |event: MouseEvent| {
+            if *drag.borrow() {
+                let cw = *canvas_width.borrow();
+                let ch = *canvas_height.borrow();
+                *dx.borrow_mut() = (event.movement_x() as f32) * 2.0 * PI / cw;
+                *dy.borrow_mut() = (event.movement_y() as f32) * 2.0 * PI / ch;
+                *theta.borrow_mut() += *dx.borrow();
+                *phi.borrow_mut() += *dy.borrow();
+            }
+        }) as Box<dyn FnMut(web_sys::MouseEvent)>);
+        event_target
+            .add_event_listener_with_callback("mousemove", mousemove_cb.as_ref().unchecked_ref())
+            .unwrap();
+        mousemove_cb.forget();
+    }
+    // RequestAnimationFrame
+    {
+        let dx = dx.clone();
+        let dy = dy.clone();
+        let drag = drag.clone();
+        // Request animation frame
+        *g.borrow_mut() = Some(Closure::wrap(Box::new(move |_d| {
+            if !*drag.borrow() {
+                *dx.borrow_mut() *= AMORTIZATION;
+                *dy.borrow_mut() *= AMORTIZATION;
+                *theta.borrow_mut() += *dx.borrow();
+                *phi.borrow_mut() += *dy.borrow();
+            }
+            drawScene(
+                &gl.clone(),
+                programm_info.clone(),
+                buffers.clone(),
+                *theta.borrow(),
+                *phi.borrow(),
+                num_vertices,
+            )
+            .unwrap();
+            request_animation_frame(f.borrow().as_ref().unwrap());
+        }) as Box<dyn FnMut(f32)>));
+
+        request_animation_frame(g.borrow().as_ref().unwrap());
+    }
+    Ok(())
+}
+
+#[allow(non_snake_case)]
+fn initShaderProgram(
+    gl: &WebGlRenderingContext,
+    vsSource: &str,
+    fsSource: &str,
+) -> Result<WebGlProgram, String> {
+    let v_shader = compile_shader(gl, WebGlRenderingContext::VERTEX_SHADER, vsSource);
+    let f_shader = compile_shader(gl, WebGlRenderingContext::FRAGMENT_SHADER, fsSource);
+
+    link_program(gl, &v_shader?, &f_shader?)
+}
+#[allow(non_snake_case)]
+fn initBuffers(
+    gl: &WebGlRenderingContext,
+    vertices: Vec<f32>,
+    num_vertices: u32,
+) -> Result<Buffers, JsValue> {
+    let positionBuffer = gl
+        .create_buffer()
+        .ok_or("failed to create positionBuffer buffer")?;
+
+    gl.bind_buffer(WebGlRenderingContext::ARRAY_BUFFER, Some(&positionBuffer));
+
+    let position_array = float_32_array!(vertices);
+    gl.buffer_data_with_array_buffer_view(
+        WebGlRenderingContext::ARRAY_BUFFER,
+        &position_array,
+        WebGlRenderingContext::STATIC_DRAW,
+    );
+
+    let indexBuffer = gl
+        .create_buffer()
+        .ok_or("failed to create indexBuffer buffer")?;
+    gl.bind_buffer(
+        WebGlRenderingContext::ELEMENT_ARRAY_BUFFER,
+        Some(&indexBuffer),
+    );
+
+    let mut indices: Vec<u16> = vec![];
+
+    for i in 0..num_vertices {
+        indices.push(i as u16);
+    }
+
+    let index_array = uint_16_array!(indices);
+    gl.buffer_data_with_array_buffer_view(
+        WebGlRenderingContext::ELEMENT_ARRAY_BUFFER,
+        &index_array,
+        WebGlRenderingContext::STATIC_DRAW,
+    );
+    Ok(Buffers(positionBuffer, indexBuffer))
+}
+#[allow(non_snake_case)]
+#[allow(dead_code)]
+fn drawScene(
+    gl: &WebGlRenderingContext,
+    programInfo: ProgramInfo,
+    buffers: Buffers,
+    theta: f32,
+    phi: f32,
+    num_vertices: u32,
+) -> Result<(), JsValue> {
+    let Buffers(positionBuffer, indexBuffer) = buffers;
+    let ProgramInfo(
+        shaderProgram,
+        vertexPosition,
+        (location_projectionMatrix, location_modelViewMatrix),
+    ) = programInfo;
+    gl.clear_color(0.0, 0.0, 0.0, 1.0);
+    gl.clear_depth(1.0);
+    gl.enable(WebGlRenderingContext::DEPTH_TEST);
+
+    gl.clear(WebGlRenderingContext::COLOR_BUFFER_BIT | WebGlRenderingContext::DEPTH_BUFFER_BIT);
+
+    let fieldOfView = 45.0 * PI / 180.0;
+    let canvas: web_sys::HtmlCanvasElement = gl
+        .canvas()
+        .unwrap()
+        .dyn_into::<web_sys::HtmlCanvasElement>()?;
+    gl.viewport(0, 0, canvas.width() as i32, canvas.height() as i32);
+    let aspect: f32 = canvas.width() as f32 / canvas.height() as f32;
+    let zNear = 1.0;
+    let zFar = 100.0;
+    let mut projectionMatrix = mat4::new_zero();
+
+    mat4::perspective(&mut projectionMatrix, &fieldOfView, &aspect, &zNear, &zFar);
+
+    let mut modelViewMatrix = mat4::new_identity();
+
+    let mat_to_translate = modelViewMatrix.clone();
+    mat4::translate(&mut modelViewMatrix, &mat_to_translate, &[-0.0, 0.0, -6.0]);
+
+    let mat_to_rotate = modelViewMatrix.clone();
+    mat4::rotate_x(&mut modelViewMatrix, &mat_to_rotate, &phi);
+    let mat_to_rotate = modelViewMatrix.clone();
+    mat4::rotate_y(&mut modelViewMatrix, &mat_to_rotate, &theta);
+
+    {
+        let numComponents = 3;
+        let type_ = WebGlRenderingContext::FLOAT;
+        let normalize = false;
+        let stride = 0;
+        let offset = 0;
+        gl.bind_buffer(WebGlRenderingContext::ARRAY_BUFFER, Some(&positionBuffer));
+
+        gl.vertex_attrib_pointer_with_i32(
+            vertexPosition,
+            numComponents,
+            type_,
+            normalize,
+            stride,
+            offset,
+        );
+        gl.enable_vertex_attrib_array(vertexPosition);
+        // gl.bind_buffer(WebGlRenderingContext::ARRAY_BUFFER, None);
+    }
+
+    gl.bind_buffer(
+        WebGlRenderingContext::ELEMENT_ARRAY_BUFFER,
+        Some(&indexBuffer),
+    );
+
+    gl.use_program(Some(&shaderProgram));
+
+    gl.uniform_matrix4fv_with_f32_array(
+        Some(&location_projectionMatrix?),
+        false,
+        &projectionMatrix,
+    );
+    gl.uniform_matrix4fv_with_f32_array(Some(&location_modelViewMatrix?), false, &modelViewMatrix);
+    {
+        let vertexCount = num_vertices as i32;
+        let type_ = WebGlRenderingContext::UNSIGNED_SHORT;
+        let offset = 0;
+        gl.draw_elements_with_i32(WebGlRenderingContext::TRIANGLES, vertexCount, type_, offset);
+    }
+
+    Ok(())
+}
+
+#[macro_export]
+macro_rules! float_32_array {
+    ($arr:expr) => {{
+        let memory_buffer = wasm_bindgen::memory()
+            .dyn_into::<WebAssembly::Memory>()?
+            .buffer();
+        let arr_location = $arr.as_ptr() as u32 / 4;
+        let array = js_sys::Float32Array::new(&memory_buffer)
+            .subarray(arr_location, arr_location + $arr.len() as u32);
+        array
+    }};
+}
+#[macro_export]
+macro_rules! uint_16_array {
+    ($arr:expr) => {{
+        let memory_buffer = wasm_bindgen::memory()
+            .dyn_into::<WebAssembly::Memory>()?
+            .buffer();
+        let arr_location = $arr.as_ptr() as u32 / 2;
+        let array = js_sys::Uint16Array::new(&memory_buffer)
+            .subarray(arr_location, arr_location + $arr.len() as u32);
+        array
+    }};
+}
+
+pub fn compile_shader(
+    context: &WebGlRenderingContext,
+    shader_type: u32,
+    source: &str,
+) -> Result<WebGlShader, String> {
+    let shader = context
+        .create_shader(shader_type)
+        .ok_or_else(|| String::from("Unable to create shader object"))?;
+    context.shader_source(&shader, source);
+    context.compile_shader(&shader);
+
+    if context
+        .get_shader_parameter(&shader, WebGlRenderingContext::COMPILE_STATUS)
+        .as_bool()
+        .unwrap_or(false)
+    {
+        Ok(shader)
+    } else {
+        Err(context
+            .get_shader_info_log(&shader)
+            .unwrap_or_else(|| String::from("Unknown error creating shader")))
+    }
+}
+
+pub fn link_program(
+    context: &WebGlRenderingContext,
+    vert_shader: &WebGlShader,
+    frag_shader: &WebGlShader,
+) -> Result<WebGlProgram, String> {
+    let program = context
+        .create_program()
+        .ok_or_else(|| String::from("Unable to create shader object"))?;
+
+    context.attach_shader(&program, vert_shader);
+    context.attach_shader(&program, frag_shader);
+    context.link_program(&program);
+
+    if context
+        .get_program_parameter(&program, WebGlRenderingContext::LINK_STATUS)
+        .as_bool()
+        .unwrap_or(false)
+    {
+        Ok(program)
+    } else {
+        Err(context
+            .get_program_info_log(&program)
+            .unwrap_or_else(|| String::from("Unknown error creating program object")))
+    }
+}
+
+pub fn window() -> web_sys::Window {
+    web_sys::window().expect("no global `window` exists")
+}
+
+pub fn request_animation_frame(f: &Closure<dyn FnMut(f32)>) {
+    window()
+        .request_animation_frame(f.as_ref().unchecked_ref())
+        .expect("should register `requestAnimationFrame` OK");
+}
+
+fn set_panic_hook() {
+    #[cfg(feature = "console_error_panic_hook")]
+    console_error_panic_hook::set_once();
+}
---
+++ src/stl.rs
@@ -0,0 +1,102 @@
+use std::slice::Iter;
+
+const STL_HEADER_BYTES: usize = 0x50;
+const STL_NUMBER_FACETS_BYTES: usize = 0x4;
+const STL_FACET_RECORD_BYTES: usize = 0x32;
+const STL_F32_BYTES: usize = 4;
+const STL_EXTRA_BYTES: usize = 2;
+const STL_AXES: usize = 3;
+const STL_VERTICES_PER_FACET: usize = 3;
+
+#[derive(Debug, Clone)]
+pub struct InvalidFileContentError {
+    msg: String,
+}
+
+impl InvalidFileContentError {
+    fn new(msg: &str) -> Self {
+        Self { msg: msg.into() }
+    }
+}
+
+impl std::fmt::Display for InvalidFileContentError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.msg)
+    }
+}
+
+pub fn get_data(bytes: &Vec<u8>) -> Result<(Vec<f32>, u32), InvalidFileContentError> {
+    let (payload, num_facets) = extract_data(bytes)?;
+    let vertices = get_vertices(payload, num_facets)?;
+    let num_vertices = STL_AXES as u32 * num_facets;
+
+    Ok((vertices, num_vertices))
+}
+
+fn get_vertices(payload: Vec<u8>, num_facets: u32) -> Result<Vec<f32>, InvalidFileContentError> {
+    let mut payload = payload.iter();
+    let mut max_value: f32 = f32::NEG_INFINITY;
+    let mut vertices: Vec<f32> = vec![];
+
+    for _ in 0..num_facets {
+        payload.by_ref().take(STL_AXES * STL_F32_BYTES).count();
+        for _ in 0..STL_AXES * STL_VERTICES_PER_FACET {
+            get_vertex(payload.by_ref(), &mut max_value, &mut vertices);
+        }
+        payload.by_ref().take(STL_EXTRA_BYTES).count();
+    }
+
+    if payload.next() != None {
+        return Err(InvalidFileContentError::new("STL: Payload is too large"));
+    }
+
+    let vertices = vertices.into_iter().map(|x| x / max_value.abs()).collect();
+
+    Ok(vertices)
+}
+
+fn get_vertex(payload: &mut Iter<u8>, max_value: &mut f32, vertices: &mut Vec<f32>) {
+    let v: Vec<u8> = payload.take(STL_F32_BYTES).cloned().collect();
+    let v: [u8; STL_F32_BYTES] = v.try_into().unwrap();
+    let v = f32::from_le_bytes(v);
+
+    if v > *max_value {
+        *max_value = v;
+    }
+    vertices.push(v);
+}
+
+fn extract_data(bytes: &Vec<u8>) -> Result<(Vec<u8>, u32), InvalidFileContentError> {
+    let mut b_it = bytes.iter();
+    let header: Vec<&u8> = b_it.by_ref().take(STL_HEADER_BYTES).collect();
+    if header.len() != STL_HEADER_BYTES {
+        return Err(InvalidFileContentError::new("STL: header too short"));
+    }
+    let num_facets: Vec<u8> = b_it
+        .by_ref()
+        .take(STL_NUMBER_FACETS_BYTES)
+        .cloned()
+        .collect();
+    if num_facets.len() != STL_NUMBER_FACETS_BYTES {
+        return Err(InvalidFileContentError::new(
+            "STL: number of facets not u32",
+        ));
+    }
+    let num_facets: Result<[u8; 4], _> = num_facets.try_into();
+    if let Err(_) = num_facets {
+        return Err(InvalidFileContentError::new(""));
+    }
+    let num_facets = u32::from_le_bytes(num_facets.unwrap());
+    let payload: Vec<_> = b_it.by_ref().cloned().collect();
+    if payload.len() % STL_FACET_RECORD_BYTES != 0 {
+        return Err(InvalidFileContentError::new(
+            "STL: payload is not aligned properly",
+        ));
+    }
+    if payload.len() / STL_FACET_RECORD_BYTES != (num_facets as usize) {
+        return Err(InvalidFileContentError::new(
+            "STL: payload does not match specified length",
+        ));
+    }
+    Ok((payload, num_facets))
+}
