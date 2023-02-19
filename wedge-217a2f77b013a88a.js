067d8ba288ea038ea69d3139829bdf166e64167b
mineiwik <mineiwik@users.noreply.github.com>
2023-02-19 14:18:35 +0000

    deploy: 802ad6591a4514da01030250b0f1c1e9a61e8aeb

--- wedge-217a2f77b013a88a.js
+++ wedge-217a2f77b013a88a.js
@@ -720,7 +720,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('wedge-878bee89355d08b8_bg.wasm', import.meta.url);
+        input = new URL('wedge-217a2f77b013a88a_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
